887d59cc7a695c0ebf8261692a4a77fe1148edde
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2022-12-14 15:55:59 +0530

    Add files via upload

---
+++ Netb/Daily_updates/Airline_reservation_system/src/main/java/com/mycompany/airline_reservation_system/Airline_reservation_system.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.airline_reservation_system;
+
+/**
+ *
+ * @author cusat
+ */
+public class Airline_reservation_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
